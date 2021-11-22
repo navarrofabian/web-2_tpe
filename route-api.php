0c1eca456f342fbede3a8ad9aa195e0b267624a8
Fabian Navarro <navarrofabian315@gmail.com>
2021-11-22 13:34:31 -0300

    refactor route-api

--- route-api.php
+++ route-api.php
@@ -2,15 +2,11 @@
 require_once 'libs/Router.php';
 require_once 'Controller/ApiCommentController.php';
 
-// crea el router
 $router = new Router();
 
-// define la tabla de ruteo
 $router->addRoute('comments', 'GET', 'ApiCommentController', 'showCommentsApi');
 $router->addRoute('comments/:ID', 'GET', 'ApiCommentController', 'getCommentById');
 $router->addRoute('comments/:ID', 'DELETE', 'ApiCommentController', 'deleteComment');
 $router->addRoute('comments', 'POST', 'ApiCommentController', 'saveComment');
-//$router->addRoute('products/:ID', 'PUT', 'ApiTaskController', 'actualizarTarea');
 
-// rutea
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
